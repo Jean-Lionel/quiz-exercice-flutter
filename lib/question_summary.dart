54e5747aa660bc8463aa3e1d939e1704ac044482
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-13 03:43:34 +0200

    question summary

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -7,30 +7,35 @@ class QuestionSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print(summaryData);
-    return Column(
-      children: summaryData
-          .map(
-            (e) => Row(
-              children: [
-                Text(
-                  ((e['question_index'] as int) + 1).toString(),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(e['question'] as String),
-                      const SizedBox(
-                        height: 30,
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData
+              .map(
+                (e) => Row(
+                  children: [
+                    Text(
+                      ((e['question_index'] as int) + 1).toString(),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(e['question'] as String),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(e['user_answer'] as String),
+                          Text(e['question_answer'] as String),
+                        ],
                       ),
-                      Text(e['user_answer'] as String),
-                      Text(e['question_answer'] as String),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-          .toList(),
+                    )
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ),
     );
   }
 }
